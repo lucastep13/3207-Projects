8fca202237e075d7cec461a24ac46336e5dcfdab
lucastep13 <sclucas13@gmail.com>
2019-09-30 09:45:39 -0400

    Creation of Project 2
    Clear function tested.

---
+++ Shell/main.cpp
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: stephenlucas
+ *
+ * Created on September 22, 2019, 8:20 PM
+ */
+
+#include <cstdlib>
+#include <string>
+#include <iostream>
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    string Input;
+    
+    for(int i=0; i<1000; i++){
+        cout<<"Clear Test\n";
+    }
+    
+    cout<< string(100, '\n');
+    
+ 
+    return 0;
+}
+
